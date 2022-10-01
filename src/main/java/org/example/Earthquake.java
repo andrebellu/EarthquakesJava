a63decd1e782969ec02dcdf47f856e67f060ec26
andrebellu <bellu.andrea@outlook.it>
2022-10-01 16:13:53 +0200

    formatted date

--- src/main/java/org/example/Earthquake.java
+++ src/main/java/org/example/Earthquake.java
@@ -2,10 +2,15 @@ package org.example;
 
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Earthquake {
     public ArrayList<String> filter(JSONObject response, double x) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+
         var features = response.getJSONArray("features");
         ArrayList<String> list = new ArrayList<>();
 
@@ -14,12 +19,13 @@ public class Earthquake {
             var properties = feature.getJSONObject("properties");
             var mag = properties.getDouble("mag");
             var place = properties.getString("place");
-            var date = properties.getLong("time");
+            var date = sdf.format(new Date(properties.getLong("time")));
 
             if (mag >= x) {
-                list.add(String.format("%s, %.1f, %s",place, mag, date));
+                list.add(String.format("Place: %s, Magnitude: %.1f, Date: %s",place, mag, date));
             }
         }
+
         return list;
     }
 }
